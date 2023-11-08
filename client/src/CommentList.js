4816e488b2d6b013420e9cc2a37e3fbe21524ab5
Shahnawaz Idariya <shahnawazidariya52@gmail.com>
2023-11-08 15:19:29 -0800

    microservices wtih event bus

---
+++ client/src/CommentList.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react'
+
+const CommentList = ({ comments }) => {
+
+    const renderedComments = comments.map(comment => {
+      let content;
+
+      if(comment.status === 'approved') {
+        content = comment.content
+      }
+
+      if(comment.status === 'pending') {
+        content = 'This comment is awaiting moderation'
+      }
+
+      if(comment.status === 'rejected') {
+        content = 'This comment has been rejected'
+      }
+  
+        return <li key={comment.id} className={comment.status==='rejected' ? 'text-danger' : ''}>{content}</li>
+    })
+  return (
+    <ul>
+    {renderedComments}
+    </ul>
+  )
+}
+
+export default CommentList
---
+++ query/index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const cors = require('cors');
+
+const app =express();
+app.use(bodyParser.json())
+app.use(cors());
+
+const posts = {};
+
+const handleEvents = (type, data) => {
+    if(type === 'PostCreated') {
+        const { id, title } = data;
+        posts[id] = { id, title, comments: [] };
+    }
+
+    if(type === 'CommentCreated') {
+        const { id, content, postId, status } = data;
+        const post = posts[postId];
+        post.comments.push({ id, content, status });
+    }
+
+    if(type === 'CommentUpdated') {
+        const { id, content, postId, status } = data;
+        const post = posts[postId];
+        const comment = post.comments.find(comment => {
+            return comment.id === id;
+        })
+
+        comment.status = status;
+        comment.content = content
+    }
+}
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+})
+
+app.post('/events', (req, res) => {
+    console.log(req.body);
+    const { type, data } = req.body;
+
+    handleEvents(type, data);
+    
+    res.send({});
+})
+
+app.listen(4002, async () => {
+    console.log('Listening on 4002');
+
+    try {
+        const res = await axios.get('http://localhost:4005/events')
+
+        for(let event of res.data) {
+            console.log('Processing event:', event.type)
+    
+            handleEvents(event.type, event.data);
+        }
+    } catch(error) {
+        console.log(error.message)
+    }
+  
+
+})
